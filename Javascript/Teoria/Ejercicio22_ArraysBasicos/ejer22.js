ecc768f6a3df3ecd7b155e4f59b5aae09e705b61
Javier Martinez <javijog@gmail.com>
2020-12-01 00:04:22 +0100

    Avanzando en arrays

---
+++ Javascript/Teoria/Ejercicio22_ArraysBasicos/ejer22.js
@@ -0,0 +1,12 @@
+'use strict'
+
+// Arrays Basicos
+
+var nombre ="Javier Martinez";
+var nombres = ["Victor Robles", "Juan Lopez", "Manolo Garcia", "Jose Martin", 52, true];
+
+var lenguajes = new Array("PHP","JS","Go", "Java");
+
+console.log(nombres);
+console.log(lenguajes);
+console.log(nombres[5]);
---
+++ Javascript/Teoria/Ejercicio23_ArraysAvanzados/ejer23.js
@@ -0,0 +1,10 @@
+'use strict'
+
+// Arrays Avanzados
+
+var nombre ="Javier Martinez";
+var nombres = ["Victor Robles", "Juan Lopez", "Manolo Garcia", "Jose Martin", 52, true];
+
+var lenguajes = new Array("PHP","JS","Go", "Java");
+
+console.log
